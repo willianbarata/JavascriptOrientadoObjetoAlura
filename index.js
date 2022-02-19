8366baeb4538befc5268a194255627f1c6a63627
willianbarata <willianbarata@gmail.com>
2022-02-19 13:08:08 -0300

    Primeira Aula

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const cliente1Nome = "ricardo";
+const cliente1CPF = 11122211107;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 22211122204;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
