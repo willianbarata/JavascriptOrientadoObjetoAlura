aa6ae082fc647363ffcb05cb844386d2c484b0a6
willianbarata <willianbarata@gmail.com>
2022-02-19 13:20:32 -0300

    Criando classe e instanciando

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+//criando a classe
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
 
-const cliente1Nome = "ricardo";
-const cliente1CPF = 11122211107;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+//instanciando e guardando em uma variável
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 22211122204;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+cliente1.nome = "ricardo";
+cliente1.cpf = 11122211107;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
 
-console.log(cliente1Nome);
+cliente2.nome = "Alice";
+cliente2.cpf = 22211122204;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1, cliente2);
+
