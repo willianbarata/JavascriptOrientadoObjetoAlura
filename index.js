9c9e475cb2f60cb940edbc0a7bea2ea7386da933
willianbarata <willianbarata@gmail.com>
2022-02-19 13:46:40 -0300

    Aula 3 - Comportamento das classes - métodos

--- index.js
+++ index.js
@@ -2,23 +2,38 @@
 class Cliente{
     nome;
     cpf;
+    
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(saldo){
+        if(this.saldo >= saldo){
+            this.saldo -= saldo;
+        }
+    }
+
 }
 
 //instanciando e guardando em uma variável
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "ricardo";
 cliente1.cpf = 11122211107;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 22211122204;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 200;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(300);
+console.log(contaCorrenteRicardo.saldo);
+
+console.log(cliente1);
+console.log(cliente2);
 
