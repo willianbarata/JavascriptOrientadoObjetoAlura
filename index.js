87c52abe42a95d8c82424e9d360c0b05702db676
willianbarata <willianbarata@gmail.com>
2022-02-19 15:32:13 -0300

    Aula 8 - Nulo e undefined

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "ricardo";
 cliente1.cpf = 11122211107;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 22211122204;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice";
+// cliente2.cpf = 22211122204;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -17,7 +17,9 @@ contaCorrenteRicardo.depositar(500);
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+//conta2.cliente = null;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
 conta2.agencia = 102;
 
 let valor = 200;
